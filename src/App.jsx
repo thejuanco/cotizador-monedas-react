29cdaddbc5cdb78056f7380752ff2b07bb888eb3
Juanco <juanffo874@gmail.com>
2024-11-16 20:43:52 -0600

    Crea el rango para el dinero y agrega estilos

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ function App() {
   return (
     <>
       <div className="my-20 max-w-lg mx-auto bg-white shadow p-10 rounded-lg">
-        <Header/>
+        <Header />
+
+        <input
+          type="range"
+          className="w-full h-6 bg-gray-200 accent-indigo-500 hover:accent-indigo-600 mt-2"
+        />
       </div>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <h1 className="text-4xl font-extrabold text-gray-500 text-center">
-      ¿Cuanto <span className="text-indigo-600">dinero</span> necesitas
+    <h1 className="text-4xl font-extrabold text-gray-600 text-center">
+      ¿Cuanto <span className="text-indigo-700">dinero</span> necesitas
     </h1>
   );
 }
