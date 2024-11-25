c12aeffa1566e4f3294c88b4a89e533837ee28b7
Juanco <juanffo874@gmail.com>
2024-11-24 21:58:02 -0600

    Actualizando la rama main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <h1 className="text-4xl font-extrabold text-gray-500 text-center">
-      ¿Cuanto <span className="text-indigo-600">dinero</span> necesitas
+      ¿Cuanto <span className="text-indigo-600">dinero</span> necesitas?
     </h1>
   );
 }
